cb3082768a5449cfe14f5b7fdf847b564795c0cb
manna <arindam@markobrando.com>
2025-05-25 18:51:29 +0530

    chatgpt final try

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,8 @@ import app from "./app";
 import connectDB from "./config/db";
 
 const PORT = process.env.PORT || 5000;
-// module.exports = app;
-app.listen(PORT, () => {
-  console.log(`Servder is running on port ${PORT}`);
-});
-// connectDB().then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`Servder is running on port ${PORT}`);
-//   });
+// app.listen(PORT, () => {
+//   console.log(`Servder is running on port ${PORT}`);
 // });
+
 module.exports = app;
-export default app;
