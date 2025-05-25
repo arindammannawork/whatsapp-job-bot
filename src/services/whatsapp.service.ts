82ee7301701fb246bfaf5f60840c073bd4f686de
manna <arindam@markobrando.com>
2025-05-25 23:46:57 +0530

    resume path

--- src/services/whatsapp.service.ts
+++ src/services/whatsapp.service.ts
@@ -47,6 +47,7 @@ export const handleIncomingMessage = async (req: Request, res: Response) => {
 
     const fileExtension = mediaType.split("/")[1]; // jpeg, png, etc.
     const filename = `image_${Date.now()}.${fileExtension}`;
+    const resumePath = path.resolve(__dirname, "asset/resume.pdf");
     let savePath: string;
     if (host === "localhost" || host === "127.0.0.1") {
       savePath = path.join(__dirname, "../../uploads", filename);
@@ -103,7 +104,7 @@ export const handleIncomingMessage = async (req: Request, res: Response) => {
           attachments: [
             {
               filename: "resume.pdf",
-              path: "./src/asset/resume.pdf", // ✅ Make sure this file exists at build time
+              path: resumePath, // ✅ Make sure this file exists at build time
             },
           ],
         });
