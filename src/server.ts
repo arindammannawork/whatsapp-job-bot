efdda16794793df7763aab4ed192cc41726e42d7
manna <arindam@markobrando.com>
2025-05-25 18:06:19 +0530

    remove vercel json

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ import connectDB from "./config/db";
 
 const PORT = process.env.PORT || 5000;
 // module.exports = app;
-// app.listen(PORT, () => {
-//   console.log(`Servder is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Servder is running on port ${PORT}`);
+});
 // connectDB().then(() => {
 //   app.listen(PORT, () => {
 //     console.log(`Servder is running on port ${PORT}`);
 //   });
 // });
-export default app;
+module.exports = app;
