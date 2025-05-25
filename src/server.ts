e23287c0da86515fa5d8a2efa8d0cfc833841ac6
manna <arindam@markobrando.com>
2025-05-25 17:34:36 +0530

    kk

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ import app from "./app";
 import connectDB from "./config/db";
 
 const PORT = process.env.PORT || 5000;
-module.exports = app;
-// app.listen(PORT, () => {
-//   console.log(`Servder is running on port ${PORT}`);
-// });
+// module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Servder is running on port ${PORT}`);
+});
 // connectDB().then(() => {
 //   app.listen(PORT, () => {
 //     console.log(`Servder is running on port ${PORT}`);
