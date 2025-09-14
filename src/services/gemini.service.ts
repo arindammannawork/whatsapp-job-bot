1aa4844a0a12832a451f219cc46ea80f88b43e24
arindam <58248399+mannamarko@users.noreply.github.com>
2025-09-14 21:05:00 +0530

    region chenge

--- src/services/gemini.service.ts
+++ src/services/gemini.service.ts
@@ -1,52 +1,3 @@
-// import { GoogleGenAI } from "@google/genai";
-
-// const ai = new GoogleGenAI({
-//   apiKey: "AIzaSyBO6fNE50z-eSnemD9y2Ccag5-yG3B6hhg",
-// });
-
-// export async function tryGoogleAi(
-//   jobDesc: string,
-//   userProfile: string,
-//   extraPromt?: string | undefined
-// ) {
-//   const prompt = `
-//   You are an assistant that writes professional job application emails.
-
-// Given the job description:
-// """${jobDesc}"""
-
-// And the applicant profile:
-// """${userProfile}"""
-
-// Write a short, formal email applying for the job. Mention resume is attached.
-
-// Use Chessy subject line and  ensure your email is not too formal.
-// dont mention the advertised platform. allways try to mention person name instead of dear hiring team or hr if not abhilable in job description then you can use hr or hiring team etc
-// allways mentiones some of my project live links
-
-// https://lastasa.vercel.app
-// https://visualizer-kohl.vercel.app
-// https://boibondhupub.com
-// https://www.autogorilla.com
-// https://credo-7y6h.vercel.app
-
-// ${extraPromt}
-
-// `;
-//   const response = await ai.models.generateContent({
-//     // model: "gemini-2.5-flash-preview-05-20",
-
-//     model: "gemini-2.0-flash",
-//     contents: prompt,
-//   });
-//   console.log(response.text);
-//   // console.log(JSON.stringify(response));
-//   // console.log(JSON.parse(response));
-
-//   // return JSON.stringify(response) || "No response";
-//   return response;
-// }
-
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { HumanMessage } from "@langchain/core/messages";
 
@@ -54,6 +5,7 @@ const chat = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-preview-05-20", // closest production-ready model
   // model: "gemini-2.0-flash", // closest production-ready model
   apiKey: process.env.GEMINI_API_KEY,
+  baseUrl: "https://us-south1-generativelanguage.googleapis.com",
 });
 
 export async function tryGoogleAi(
